b366b07a96c7ba105d8464bf6e5c459e01ac9f54
Hazem Wael <hazemwael0000@gmail.com>
2026-01-13 20:01:33 +0200

    Moved assets out from lib folder + renamed the app for android

--- lib/models/Cart_model.dart
+++ lib/models/Cart_model.dart
@@ -3,22 +3,22 @@ import 'package:flutter/material.dart';
 class Cartmodel extends ChangeNotifier {
   // list of items on sale
   final List _shopitems = [
-    ["apple", "2.00", "lib/images/apple.png"],
-    ["avocado", "6.00", "lib/images/avocado.png"],
-    ["banana", "1.50", "lib/images/banana.png"],
-    ["blackberry", "7.50", "lib/images/blackberry.png"],
-    ["cherries", "6.50", "lib/images/cherries.png"],
-    ["coconut", "10.00", "lib/images/coconut.png"],
-    ["figs", "3.00", "lib/images/figs.png"],
-    ["grapes", "3.50", "lib/images/grapes.png"],
-    ["kiwi", "4.50", "lib/images/kiwi.png"],
-    ["mango", "2.67", "lib/images/mango.png"],
-    ["orange", "1.69", "lib/images/orange.png"],
-    ["peach", "3.50", "lib/images/peach.png"],
-    ["pear", "7.50", "lib/images/pear.png"],
-    ["pineapple", "9.00", "lib/images/pineapple.png"],
-    ["strawberry", "6.50", "lib/images/strawberry.png"],
-    ["watermelon", "6.00", "lib/images/watermelon.png"],
+    ["apple", "2.00", "assets/images/apple.png"],
+    ["avocado", "6.00", "assets/images/avocado.png"],
+    ["banana", "1.50", "assets/images/banana.png"],
+    ["blackberry", "7.50", "assets/images/blackberry.png"],
+    ["cherries", "6.50", "assets/images/cherries.png"],
+    ["coconut", "10.00", "assets/images/coconut.png"],
+    ["figs", "3.00", "assets/images/figs.png"],
+    ["grapes", "3.50", "assets/images/grapes.png"],
+    ["kiwi", "4.50", "assets/images/kiwi.png"],
+    ["mango", "2.67", "assets/images/mango.png"],
+    ["orange", "1.69", "assets/images/orange.png"],
+    ["peach", "3.50", "assets/images/peach.png"],
+    ["pear", "7.50", "assets/images/pear.png"],
+    ["pineapple", "9.00", "assets/images/pineapple.png"],
+    ["strawberry", "6.50", "assets/images/strawberry.png"],
+    ["watermelon", "6.00", "assets/images/watermelon.png"],
   ];
   // list of cart items
   final List _cartitems = [];
